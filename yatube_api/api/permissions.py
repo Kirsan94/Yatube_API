7c812e9d12395de71e29a71df5dd2b61e1d328fd
Лев Харьков <kirsan94@mail.ru>
2022-04-16 19:33:11 +0300

    Первая попытка

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -8,9 +8,9 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-            )
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
