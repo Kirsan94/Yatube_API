e0aab94cd8ff754e178f7ebadb3ac33a27c24e80
Лев Харьков <kirsan94@mail.ru>
2022-04-16 20:18:51 +0300

    Вторая попытка

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -7,15 +7,15 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Yatube API",
-      default_version='v1',
-      description="Документация для приложения API проекта Yatube",
-      contact=openapi.Contact(email="kirsan94@yandex.ru"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Yatube API",
+        default_version='v1',
+        description="Документация для приложения API проекта Yatube",
+        contact=openapi.Contact(email="kirsan94@yandex.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -30,10 +30,10 @@ urlpatterns = [
 
 
 urlpatterns += [
-   url(r'^api/v1/swagger(?P<format>\.json|\.yaml)$',
-       schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^api/v1/swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-       name='schema-swagger-ui'),
-   url(r'^api/v1/redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-       name='schema-redoc'),
+    url(r'^api/v1/swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^api/v1/swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^api/v1/redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
 ]
